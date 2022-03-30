ed022860073f2744f56091c2a37ac0ca6279895e
Nguyen Hoang <hoangnplph16663@fpt.edu.vn>
2022-03-30 11:11:56 +0700

    updateE

---
+++ BUS_BussinessLayer/iBUS_Services/iRoleServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_DataAccessLayer.Entities;
+
+namespace BUS_BussinessLayer.iBUS_Services
+{
+    public interface iRoleServices
+    {
+        string AddRoles(Roles role);
+        string UpdateRoles(Roles role);
+        string RemoveRoles(Roles role);
+        Roles GetRoleByID(string id);
+        List<Roles> GetListRole();
+    }
+}
